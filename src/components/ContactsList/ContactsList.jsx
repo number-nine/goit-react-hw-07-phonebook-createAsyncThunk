c4b1ff03289d321828e25edb175b203167a50e1f
number-nine <sergii.pokhylko@gmail.com>
2023-06-18 11:54:47 +0300

    cleaned

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -2,38 +2,27 @@ import { useSelector, useDispatch } from 'react-redux';
 import * as contactsAPI from 'redux/contactOperations';
 import {
   selectAuth,
-  // selectIsLoading,
-  // selectContacts,
   selectVisibleContacts,
 } from 'redux/selectors';
 
-// import { remove } from 'redux/contactsSlice';
 import {
   ListWrapper,
   PrivateContact,
   SharedContact,
 } from './ContactsList.styled';
 import { Button } from '../common.styled';
-// import SplashScreen from 'components/SplashScreen';
 
 
 const ContactsList = () => {
-  // const isLoading = useSelector(selectIsLoading);
   const { isLoggedIn } = useSelector(selectAuth);
   const visibleContacts = useSelector(selectVisibleContacts);
-
-  // const visibleContacts = contacts.filter(
-  //   contact =>
-  //     contact.name.toLowerCase().includes(filter.trim().toLowerCase()) &&
-  //     (!contact.isPrivate || isLoggedIn)
-  // );
   const dispatch = useDispatch();
+
   return visibleContacts.length === 0 ? (
     <p>Nothing to show</p>
   ) : (
     <ListWrapper>
       {visibleContacts.map(({ id, name, phone, isPrivate }) => {
-        // console.log(isPrivate);
         return (
           <li key={id}>
             {name}: {phone}
@@ -51,7 +40,6 @@ const ContactsList = () => {
                  Delete
               </Button>
             )}
-            {/* {isLoading && <SplashScreen/>} */}
           </li>
         );
       })}
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,28 +1,20 @@
 import { createSelector } from '@reduxjs/toolkit';
 
-// export const selectAuth = state => {
-//   console.log('selectAuth');
-//   return state.auth;
-// };
+
 const selfSelector = state => state;
 const selectContacts = state => state.contacts.items;
+
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
-
 export const selectFilter = state => state.filter;
 
 export const selectAuth = createSelector([selfSelector], state => {
-  console.log('selectAuth');
   return state.auth;
 });
 
 export const selectVisibleContacts = createSelector(
   [selectFilter, selectAuth, selectContacts],
   (filter, { isLoggedIn }, items) => {
-    //   const filter = selectFilter(state);
-    //   const { isLoggedIn } = selectAuth(state);
-    //   const items = selectContacts(state);
-    console.log('selectVisibleContacts');
     return items.filter(
       contact =>
         contact.name.toLowerCase().includes(filter.trim().toLowerCase()) &&
